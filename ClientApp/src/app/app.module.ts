bbee57aa411a43fefb12ed6c8f2b3db0f75ac4d9
Choonyin Yeap <cyyeap.pg@gmail.com>
2018-12-07 21:27:38 +1100

    create board component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SuitComponent } from './suit/suit.component';
+import { BoardComponent } from './board/board.component';
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { SuitComponent } from './suit/suit.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    SuitComponent
+    SuitComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
