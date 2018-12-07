697a9b130aeee80ee7d81cba43aa10d9ed98d1d2
Choonyin Yeap <cyyeap.pg@gmail.com>
2018-12-07 21:27:04 +1100

    initialise flag and suit

--- ClientApp/src/app/models/suit.ts
+++ ClientApp/src/app/models/suit.ts
@@ -1,5 +1,8 @@
-export class Suit {
-    number: number;
+import { SuitFlag } from "../suit-flag.enum";
+import { NumberFlag } from "../number-flag.enum";
 
+export class Suit {
+    number: NumberFlag;
+    suitFlag: SuitFlag;
 }
 
---
+++ ClientApp/src/app/suit-flag.enum.ts
@@ -0,0 +1,7 @@
+export enum SuitFlag {
+    None = 0, 
+    Dot = 1 << 0, 
+    Bamboo = 1 << 1, 
+    Character = 1 << 3
+}
+
