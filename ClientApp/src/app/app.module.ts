37a4c16dcc5744120d9224d80e9cd8c60cc6bf96
Choonyin Yeap <cyyeap.pg@gmail.com>
2018-12-11 23:34:42 +1100

    added drag and drop package and added tutorial component

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
@@ -11,6 +12,7 @@ import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { SuitComponent } from './suit/suit.component';
 import { BoardComponent } from './board/board.component';
+import { TutorialComponent } from './tutorial/tutorial/tutorial.component';
 
 @NgModule({
   declarations: [
@@ -20,17 +22,20 @@ import { BoardComponent } from './board/board.component';
     CounterComponent,
     FetchDataComponent,
     SuitComponent,
-    BoardComponent
+    BoardComponent,
+    TutorialComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    DragDropModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
       { path: 'board', component: BoardComponent },
+      { path: 'tutorial', component: TutorialComponent },
     ])
   ],
   providers: [],
